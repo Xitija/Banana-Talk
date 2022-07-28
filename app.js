40d13d822f7457fc78ccbed2d0db5d499f670d64
Kshitija Kadam <kshitijakadam04@gmail.com>
2022-07-28 23:35:03 +0530

    chore : Added code for minion app

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 
+var coll = document.querySelector(".collapsible");
+// var content = this.nextElementSibling;
+var content = document.querySelector(".content");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+coll.addEventListener("click", function() {
+    // The classList property returns the CSS classnames of an element.
+    // Toggle between adding and removing a class name from an element with JavaScript.
+
+    this.classList.toggle("active"); // try another class
+    // console.log(this);
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+
+function getTranslationURL(text){
+    return serverURL + "?text=" + text;
+}
+
+function errorHandler(err){
+    console.log("An error occured", err);
+    alert("Something went wrong , please try after sometime");
+}
+
+function clickHandler(){
+  var inputText = txtInput.value;
+  if(inputText.trim()){
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => outputDiv.innerHTML = json.contents.translated)
+    .catch(errorHandler)
+  } else {
+    outputDiv.innerHTML = "";
+  }
+}
+
+btnTranslate.addEventListener("click",clickHandler);
